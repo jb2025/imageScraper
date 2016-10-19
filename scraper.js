f49ca254b3ebfbf0d2bc5a869a92796654ace8a9
jb2025 <jb2025@users.noreply.github.com>
2016-10-19 17:51:32 -0400

    Create scraper.js

---
+++ scraper.js
@@ -0,0 +1,29 @@
+//When running this file, go to the directory you want to scrape to
+//Requires Node.js, prior to running code run the two commands below in the shell
+//npm install request cheerio 
+//npm install jQuery
+
+var request = require('request'),
+	cheerio = require('cheerio'),
+	fs = require ('fs'),
+	urls = [];
+
+
+request('https://imgur.com/r/soccer', function(err, resp, body){
+	if(!err && resp.statusCode == 200){
+		var $ = cheerio.load(body);
+		
+		//a.title refers to the a href class, using the nomenclature a.id
+		//#imagelist below refers to the div id, allowing you to search within a specific area
+		$('a.image-list-link', '#imagelist').each(function() {
+			var url = this.attr('href');
+			urls.push(url);
+		});
+		console.log(urls);
+		for(var i = 0; i < urls.length; i++){
+		request(urls[i]).pipe(fs.createWriteStream('img/' + i + '.jpg'));	
+		}
+		
+	}
+
+});
